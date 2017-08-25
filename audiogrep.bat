70eacd51cc9bd092f7ef20e7057dc4615f766859
kelciour <kelciour@gmail.com>
2017-08-25 23:36:59 +0300

    Add 'quit' as exit command

--- audiogrep.bat
+++ audiogrep.bat
@@ -4,15 +4,17 @@ setlocal EnableDelayedExpansion
 set media_dir="J:\English AudioBooks"
 
 for /L %%n in (1,0,1) do (
-  set /p phrase="Phrase: "
-  if /I "!phrase!"=="q" call :stop
-  if /I "!phrase!"=="x" call :stop
-  if /I "!phrase!"=="exit" call :stop
-  
-  rem Create a window even if there is no album cover art and disable album cover art.
-  rem python playphrase.py --mpv-options "--video=no --force-window=yes --osc=no --title=${filename}" --input %media_dir% "!phrase!"
+    set /p phrase="Phrase: "
 
-  python playphrase.py --mpv-options "--sub-font-size=37 --sub-back-color=0.05/0.9 --sub-scale-by-window=no --sub-scale-with-window=no --autofit=620 --osc=no --title=${filename}" --input %media_dir% "!phrase!"
+    if /I "!phrase!"=="q" call :stop
+    if /I "!phrase!"=="x" call :stop
+    if /I "!phrase!"=="quit" call :stop
+    if /I "!phrase!"=="exit" call :stop
+
+    rem Disable album cover art and create a window even if there is no album cover art.
+    rem python playphrase.py --mpv-options "--video=no --force-window=yes --osc=no --title=${filename}" --input "%media_dir%" "!phrase!"
+
+    python playphrase.py --mpv-options "--sub-font-size=37 --sub-back-color=0.05/0.9 --sub-scale-by-window=no --sub-scale-with-window=no --autofit=620 --osc=no --title=${filename}" --input "%media_dir%" "!phrase!"
 )
 
 :stop
--- videogrep.bat
+++ videogrep.bat
@@ -4,11 +4,14 @@ setlocal EnableDelayedExpansion
 set media_dir="J:\English Movies"
 
 for /L %%n in (1,0,1) do (
-  set /p phrase="Phrase: "
-  if /I "!phrase!"=="q" call :stop
-  if /I "!phrase!"=="x" call :stop
-  if /I "!phrase!"=="exit" call :stop
-  python playphrase.py --input %media_dir% "!phrase!"
+    set /p phrase="Phrase: "
+    
+    if /I "!phrase!"=="q" call :stop
+    if /I "!phrase!"=="x" call :stop
+    if /I "!phrase!"=="quit" call :stop
+    if /I "!phrase!"=="exit" call :stop
+
+    python playphrase.py --input "%media_dir%" "!phrase!"
 )
 
 :stop
