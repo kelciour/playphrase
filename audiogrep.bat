9f0c4a60913717ca75bae1b225bc326213b73a6f
kelciour <kelciour@gmail.com>
2017-08-26 13:00:57 +0300

    Update subtitles background color

--- audiogrep.bat
+++ audiogrep.bat
@@ -14,7 +14,7 @@ for /L %%n in (1,0,1) do (
     rem Disable album cover art and create a window even if there is no album cover art.
     rem python playphrase.py --mpv-options "--video=no --force-window=yes --osc=no --title=${filename}" --input %media_dir% "!phrase!"
 
-    python playphrase.py --mpv-options "--sub-font-size=37 --sub-back-color=0.05/0.9 --sub-scale-by-window=no --sub-scale-with-window=no --autofit=620 --osc=no --title=${filename}" --input %media_dir% "!phrase!"
+    python playphrase.py --mpv-options "--sub-font-size=37 --sub-back-color=0.05/0.95 --sub-scale-by-window=no --sub-scale-with-window=no --autofit=620 --osc=no --title=${filename}" --input %media_dir% "!phrase!"
 )
 
 :stop
